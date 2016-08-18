a2944eb9a0e7462276e5b7a362405f9a340dac10
MertcanGokgoz <mertcan.gokgoz@gmail.com>
2016-08-19 01:40:34 +0300

    last update

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-    var siteUrl = 'http://'+(document.location.hostname||document.location.host);
+    var siteUrl = 'https://'+(document.location.hostname||document.location.host);
     $(document).delegate('a[href^="/"],a[href^="'+siteUrl+'"]', "click", function(e) {
         e.preventDefault();
         History.pushState({}, "", this.pathname);
